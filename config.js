ceb719e98bc9e15cadaa143ba2075acb9ae68458
Breaker59 <72879774+Breaker59@users.noreply.github.com>
2020-10-14 18:09:24 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "NzY1OTIwMTU3Mzk4NTk3NjY0.X4b0uw.jevirBu7CQLEwZcjD5ewdWxWycg",
+    prefix: "!",
+    owner: "765920157398597664",
+    corona_api: "https://corona.lmao.ninja/v2"
+    }
